00c50ec546f0886fecb3c8554a9dd2fd1d47e2bb
Arnaud <arnaud.maitre@he-arc.ch>
2024-10-31 10:08:40 +0100

    Fait 5

--- main.js
+++ main.js
@@ -1,11 +1,30 @@
 const app = Vue.createApp({
-    data: function () {
-      return {
-        title: 'Achat de café Nespresso',
-        description: '[Une description]',
-        image: 'assets/images/colombia.png',
-        inStock: 8,
-        onSale: true,
-      };
-    },
-  });
+  data: function () {
+    return {
+      title: 'Achat de café Nespresso',
+      description: 'Café Colombia',
+      image: 'assets/images/colombia.png',
+      inStock: 8,
+      onSale: true,
+      details: ['Doux', 'Harmonieux'],
+      carouselImages: [
+        {
+          id: 1,
+          text: 'Capsule 1',
+        },
+        {
+          id: 2,
+          text: 'Capsule 2',
+        },
+        {
+          id: 3,
+          text: 'Tasse',
+        },
+        {
+          id: 4,
+          text: 'Paquet',
+        }
+      ],
+    };
+  },
+});
